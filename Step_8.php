ad8550038a6168df05c893989a597b90c9c75770
Oussama-Ghomrawi <ouzz1.96@gmail.com>
2022-02-24 12:42:07 +0200

    Step_8_A

---
+++ Step_8.php
@@ -0,0 +1,8 @@
+<?php
+$grocery[1]="Eggs";
+$grocery[2]="Milk";
+$grocery[3]="Cheese";
+$grocery[4]="WaterPack";
+$grocery[5]="Tissues";
+$grocery[6]="Watermelon";
+echo "Hello Sir, do you have $grocery[1], $grocery[2], and $grocery[3]? Also if you sell fruits can I find a $grocery[6]?\n";
