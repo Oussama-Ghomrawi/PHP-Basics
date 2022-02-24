61febcf50217c6cfd27ba287ecdd7e80a3b1561a
Oussama-Ghomrawi <ouzz1.96@gmail.com>
2022-02-24 13:46:56 +0200

    Step_8_B

--- Step_6.php
+++ Step_6.php
@@ -1,10 +1,19 @@
+
+//function calculateArea(){
+//    $height=5;
+//    $width=3;
+//    $area= $height*$width;
+//    return $area;
+//};
+//$area = calculateArea();
+//echo "Area is ${area}\n";
+
 <?php
-function calculateArea(){
-    $height=5;
-    $width=3;
-    $area= $height*$width;
-    return $area;
-};
-$area = calculateArea();
-echo "Area is ${area}\n";
+    $height = 0;
+    $width = 0;
+    function calculateArea($height, $width) {
+        $area = $height * $width;
+        echo $area;
+    };
+    calculateArea(5,3)
 ?>
--- Step_8.php
+++ Step_8.php
@@ -1,4 +1,5 @@
 <?php
+//PART A
 $grocery[1]="Eggs";
 $grocery[2]="Milk";
 $grocery[3]="Cheese";
@@ -6,3 +7,11 @@ $grocery[4]="WaterPack";
 $grocery[5]="Tissues";
 $grocery[6]="Watermelon";
 echo "Hello Sir, do you have $grocery[1], $grocery[2], and $grocery[3]? Also if you sell fruits can I find a $grocery[6]?\n";
+
+//PART B
+$groceries = array (
+    array("eggs","balade","mazare3"),
+    array("milk","Fresh","Taanayel"),
+    array("water-pack","Tanoureen","Reem"),
+  );
+  echo "Hey Sir, Please I need 1 pack of " .$groceries[0][1]." ".$groceries[0][0]. " and 3 ".$groceries[2][2]. " ".$groceries[2][0]. "\n";
